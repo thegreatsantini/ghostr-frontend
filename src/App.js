39db30a5b63d6fe030f6667929049628c0a0e2ed
cippero <gilwein@gmail.com>
2018-05-08 17:06:12 -0700

    initial branching

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   state = { users: [] }
 
   componentDidMount() {
-    fetch('/users')
+    fetch('http://localhost:8080/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
