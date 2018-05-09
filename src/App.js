a00f5994790ab1e42cdc700b0c83d9ec8a4f81e3
thegreatsantini <lss13@my.fsu.edu>
2018-05-09 11:00:05 -0700

    trying to keep up with 3.0

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import Profile from './componets/Profile'
 import './App.css';
 
 class App extends Component {
-  state = { users: [] }
+  constructor() {
+    super()
+    this.state = {
+      users : []
+    }
+  }
 
   componentDidMount() {
-    // fetch('http://localhost:8080/users')
+    // fetch('http://localhost:8080/fe')
     //   .then(res => res.json())
-    //   .then(users => this.setState({ users }));
-
-    fetch('http://localhost:8080/fe')
-      .then(res => res.json())
-      .then(data => {
-        window.location = data.redirect;
-      });
+    //   .then(data => {
+    //     window.location = data.redirect;
+    //   });
   }
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={(props) => <Home user={currentUser} />} />
+        <Route exact path='/' component={(props) => <Home  />} />
         <Route path='/profile' component={(props) => <Profile />} />
       </Switch>
     );
