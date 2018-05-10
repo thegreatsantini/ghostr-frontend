5650dd8da59c3d3b47dd36aba2223afd275f56c0
cippero <gilwein@gmail.com>
2018-05-10 10:43:15 -0700

    oauth integration

--- src/App.js
+++ src/App.js
@@ -6,18 +6,49 @@ import {
 import Home from './componets/Home'
 import Profile from './componets/Profile'
 import './App.css';
+import TwitterLogin from 'react-twitter-auth';
 
 class App extends Component {
-  state = { users: [] }
+  //state = { users: [] }
+  constructor(props) {
+    super(props);
+    this.state = { isAuthenticated: false, user: null, token: ''};
+  }
 
-  componentDidMount() {
-    fetch('http://localhost:8080/users')
-      .then(res => res.json())
-      .then(users => {
-        //this.setState({ users });
-        console.log(users);
-      })
-      .then(data => console.log(data));
+  onSuccess = (response) => {
+    console.log('success');
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({isAuthenticated: true, user: user, token: token});
+      }
+    });
+  };
+
+  onFailed = (error) => {
+    console.log('fail');
+    alert(error);
+  };
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  };
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     console.log(this.state.user);
+  //   }, 3000);
+    
+  // }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:8080/users')
+  //     .then(res => res.json())
+  //     .then(users => {
+  //       //this.setState({ users });
+  //       console.log(users);
+  //     })
+  //     .then(data => console.log(data));
 
     // fetch('http://localhost:8080/fe')
     //   .then(res => res.json())
@@ -25,15 +56,51 @@ class App extends Component {
     //     //window.location = data.redirect;
     //     console.log(data);
     //   });
-  }
+  // }
+  // render() {
+  //   return (
+  //     <Switch>
+  //       <Route exact path='/' component={(props) => <Home />} />
+  //       <Route path='/profile' component={(props) => <Profile />} />
+  //     </Switch>
+  //   );
+  // }
+
+  // <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+  //                   onFailure={this.onFailed} onSuccess={this.onSuccess}
+  //                   requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
+
+    //   <TwitterLogin loginUrl="http://localhost:8080/auth/twitter"
+    //                 onFailure={this.onFailed} onSuccess={this.onSuccess}
+    //                 requestTokenUrl="http://localhost:8080/twitter/reverse"/>
+    // );
+
   render() {
-    return (
-      <Switch>
-        <Route exact path='/' component={(props) => <Home />} />
-        <Route path='/profile' component={(props) => <Profile />} />
-      </Switch>
+    let content = !!this.state.isAuthenticated ?
+    (
+      <div>
+        <p>Authenticated</p>
+        <div>
+          {this.state.user.email} {/* change email to name */}
+        </div>
+        <div>
+          <button onClick={this.logout} className="button" >
+            Log out
+          </button>
+        </div>
+      </div>
+    ) :
+    (
+  <TwitterLogin loginUrl="http://localhost:8080/auth/twitter"
+                onFailure={this.onFailed} onSuccess={this.onSuccess}
+                requestTokenUrl="http://localhost:8080/auth/twitter/reverse"/>
     );
-  }
-}
+
+  return (
+    <div className="App">
+      {content}
+    </div>
+  );
+}}
 
 export default App;
