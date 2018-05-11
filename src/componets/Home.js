ef1b936cfe79d6b1dbb83472e796fccae21a8d83
thegreatsantini <lss13@my.fsu.edu>
2018-05-11 13:44:14 -0700

    removed sarch bar

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -9,7 +9,6 @@ import {
     ListGroupItem,
 } from 'reactstrap'
 import '../Home.css'
-import Search from './Search'
 
 class Home extends Component {
     constructor(props) {
@@ -55,8 +54,7 @@ class Home extends Component {
                     <hr />
                     <div>
                         <Link to='/profile'><Button color="primary">Log In</Button></Link>
-                        <Button color="primary"><a href='http://localhost:8080/sessions/connect'>Twitter</a></Button>  
-                        <Search />
+                        <Button color="primary"><a href='http://localhost:8080/sessions/connect'>Twitter</a></Button> 
                     </div>
                 </div>
             </div>
