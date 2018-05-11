19893a2654413240ad729fdf5d5c342fd06a3e83
cippero <gilwein@gmail.com>
2018-05-11 14:00:11 -0700

    setting up new oauth

--- src/App.js
+++ src/App.js
@@ -6,63 +6,18 @@ import {
 import Home from './componets/Home'
 import Profile from './componets/Profile'
 import './App.css';
-// import TwitterLogin from 'react-twitter-auth';
-// import Autosuggest from 'react-autosuggest';
 
-// const languages = [
-//   {
-//     name: 'C',
-//     year: 1972
-//   },
-//   {
-//     name: 'Elm',
-//     year: 2012
-//   }
-// ];
-
-// // Teach Autosuggest how to calculate suggestions for any given input value.
-// const getSuggestions = value => {
-//   const inputValue = value.trim().toLowerCase();
-//   const inputLength = inputValue.length;
-
-//   return inputLength === 0 ? [] : languages.filter(lang =>
-//     lang.name.toLowerCase().slice(0, inputLength) === inputValue
-//   );
-// };
-
-// // When suggestion is clicked, Autosuggest needs to populate the input
-// // based on the clicked suggestion. Teach Autosuggest how to calculate the
-// // input value for every given suggestion.
-// const getSuggestionValue = suggestion => suggestion.name;
-
-// // Use your imagination to render suggestions.
-// const renderSuggestion = suggestion => (
-//   <div>
-//     {suggestion.name}
-//   </div>
-// );
-
-//     // Autosuggest is a controlled component.
-//     // This means that you need to provide an input value
-//     // and an onChange handler that updates this value (see below).
-//     // Suggestions also need to be provided to the Autosuggest,
-//     // and they are initially empty because the Autosuggest is closed.
 class App extends Component {
-<<<<<<< HEAD
-  state = { 
-    user: [],
-    userName: '' 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isAuthenticated: false, 
+      user: null, 
+      token: ''
+    };
   }
 
   componentDidMount() {
-    // fetch('http://localhost:8080/users')
-    //   .then(res => res.json())
-    //   .then(users => {
-    //     //this.setState({ users });
-    //     console.log(users);
-    //   })
-    //   .then(data => console.log(data));
-
     fetch('http://localhost:8080/')
       .then(res => res.json())
       .then(data => {
@@ -75,113 +30,34 @@ class App extends Component {
           })
         }
       });
-=======
-  constructor(props) {
-    super(props);
-    this.state = { 
-      isAuthenticated: false, 
-      user: null, 
-      token: ''
-      // value: '',
-      // suggestions: []
-    };
   }
 
-  onSuccess = (response) => {
-    console.log('success');
-    const token = response.headers.get('x-auth-token');
-    response.json().then(user => {
-      if (token) {
-        this.setState({isAuthenticated: true, user: user, token: token});
-      }
-    });
-  };
-
-  onFailed = (error) => {
-    console.log('fail');
-    alert(error);
-  };
-
-  logout = () => {
-    this.setState({isAuthenticated: false, token: '', user: null})
-  };
-
-  // onChange = (event, { newValue }) => {
-  //   this.setState({
-  //     value: newValue
+  // onSuccess = (response) => {
+  //   console.log('success');
+  //   const token = response.headers.get('x-auth-token');
+  //   response.json().then(user => {
+  //     if (token) {
+  //       this.setState({isAuthenticated: true, user: user, token: token});
+  //     }
   //   });
   // };
 
-  // // Autosuggest will call this function every time you need to update suggestions.
-  // // You already implemented this logic above, so just use it.
-  // onSuggestionsFetchRequested = ({ value }) => {
-  //   this.setState({
-  //     suggestions: getSuggestions(value)
-  //   });
+  // onFailed = (error) => {
+  //   console.log('fail');
+  //   alert(error);
   // };
 
-  // // Autosuggest will call this function every time you need to clear suggestions.
-  // onSuggestionsClearRequested = () => {
-  //   this.setState({
-  //     suggestions: []
-  //   });
+  // logout = () => {
+  //   this.setState({isAuthenticated: false, token: '', user: null})
   // };
 
-    
-
-  // componentDidMount() {
-  //   fetch('http://localhost:8080/users')
-  //     .then(res => res.json())
-  //     .then(users => {
-  //       //this.setState({ users });
-  //       console.log(users);
-  //     })
-  //     .then(data => console.log(data));
-
-    // fetch('http://localhost:8080/fe')
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     //window.location = data.redirect;
-    //     console.log(data);
-    //   });
-  // }
-  componentDidMount() {
-    fetch('http://localhost:8080/users')
-      .then(res => {
-        console.log(res.json())
-        // res.json()
-      } )
-      .then(seededUsers => {
-        // this.setState({ users: seededUsers[0] });
-      })
-      // .then(() => console.log(this.state.users))
->>>>>>> 64248bca9a188bec945b3e506d88e173eab153ca
-  }
   render() {
-    // const { value, suggestions } = this.state;
-
-    // // Autosuggest will pass through all these props to the input.
-    // const inputProps = {
-    //   placeholder: 'Type a programming language',
-    //   value,
-    //   onChange: this.onChange
-    // };
-
-    // // Finally, render it!
     return (
       <div>
         <Switch>
           <Route exact path='/' component={(props) => <Home />} />
           <Route path='/profile' component={(props) => <Profile />} />
         </Switch>
-        {/*<Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />*/}
       </div>
     );
   }
